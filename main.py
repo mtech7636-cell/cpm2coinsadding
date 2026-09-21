1f9f9569a4c24b7baca686d13f558f63285f2367
mtech7636-cell <mtech7636@gmail.com>
2026-09-21 07:43:11 +0530

    Reduce batch size from 100 to 30

--- main.py
+++ main.py
@@ -389,7 +389,7 @@ async def phoenix_farm_engine(token, uid, session, progress_callback=None):
                         pass
         return coins
 
-    batch_size = 100
+    batch_size = 30
     for i in range(0, len(all_combos), batch_size):
         batch = all_combos[i:i + batch_size]
         results = await asyncio.gather(
